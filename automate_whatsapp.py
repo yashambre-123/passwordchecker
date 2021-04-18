1afa343b0946f2f7fce91baa597ba8c3df421634
yashambre-123 <82760930+yashambre-123@users.noreply.github.com>
2021-04-18 20:52:34 +0530

    Update automate_whatsapp.py

--- automate_whatsapp.py
+++ automate_whatsapp.py
@@ -1,5 +1,8 @@
 import pywhatkit as whatsapp
 
-whatsapp.sendwhatmsg('+919789668950','sorry , mom',23,55)
+whatsapp.sendwhatmsg('+91','',,) # Add your number in the first single quotes. +91 is for number of an user from India, if you are from other country change +91
+# In the second single quotes, write the message that you want to send.
+# In the third space, add the hour at which you want to send the message.
+# In the fourth space, add the minute at which you want to send your message.
 
 
